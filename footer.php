ce8f0b5cb2ecd756975547d6365f03fd02543cf5
Lynn Wallenstein <lynn.wallenstein@livingsocial.com>
2012-04-16 10:38:59 -0400

    Updated libs to vendor

--- footer.php
+++ footer.php
@@ -33,7 +33,7 @@
 
   <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="<?php bloginfo('template_url'); ?>/js/libs/jquery-1.7.2.min.js"><\/script>')</script>
+  <script>window.jQuery || document.write('<script src="<?php bloginfo('template_url'); ?>/js/vendor/jquery-1.7.2.min.js"><\/script>')</script>
 
   <!-- scripts concatenated and minified via ant build script-->
   <script src="<?php bloginfo('template_url'); ?>/js/plugins.js"></script>
